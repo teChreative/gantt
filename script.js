6e4fac12f87f3016a504e39f532c38837a081525
techreative <nathanielejones@hotmail.com>
2021-10-05 15:23:28 -0400

    tired to add user input feature

--- script.js
+++ script.js
@@ -36,5 +36,72 @@ function createChart(e) {
 
 }
 
+// window.addEventListener("load", createChart); (keep here for when add-ons below are removed)
+// window.addEventListener("resize", createChart);
+
+// ADD-ONS START HERE
+
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var taskFormHandler = function (event) {
+  event.preventDefault();
+
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var dayStartInput = document.querySelector("select[name='day-start']").value;
+  var dayendInput = document.querySelector("select[name='day-end']").value;
+
+  // check if input values are empty strings
+  if (!dayStartInput || !dayendInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
+  formEl.reset();
+
+  // package up data as an object
+  var taskDataObj = {
+    task: taskNameInput,
+    start: dayStartInput,
+    end: dayendInput
+  };
+
+  // send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
+
+
+
+var createTaskEl = function (taskDataObj) {
+  // create list item
+  var listItemEl = document.createElement("ul");
+  listItemEl.className = "chart-bars";
+  let left = 0, width = 0;
+  
+
+  // IMPLANT START from createChart for repurpose
+  listItemEl.style.left = `${left}px`;
+  listItemEl.style.width = `${width}px`;
+  listItemEl.style.opacity = .5;
+  //background-color: rgb(51, 168, 165); opacity: 1;
+  // if (listItemEl.type == "load") {
+  //   listItemEl.style.backgroundColor = listItemEl.dataset.color;
+  //   listItemEl.style.opacity = 1;
+  // }
+  // IMPLANT END from createChart for repurpose
+  
+
+  // create element to hold task info and add to list item
+  // STYLE NEEDS TO DYNAMICALLY GENERATE THE left AND width PROPERTIES: 
+  listItemEl.innerHTML = `<li data-duration="wed-thu" data-color="#b03532" class="task-item" style="left: 103px; width: 404px; background-color: rgb(51, 168, 165);">${taskDataObj.task}</li>`;
+  // listItemEl.innerHTML = `<li data-duration="${USER IN}" data-color="#b03532" class="task-item" style="left: ${CODE GEN}; width: ${CODE GEN}; background-color: rgb(51, 168, 165); opacity: 1;">${taskDataObj.task}</li>`;
+  
+  //listItemEl.appendChild();
+
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
+  
+};
+
+formEl.addEventListener("submit", taskFormHandler);
 window.addEventListener("load", createChart);
 window.addEventListener("resize", createChart);
