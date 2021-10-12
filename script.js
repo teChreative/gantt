283e3a07a0bc35baf5e60b5f2f1dffaa132182c7
techreative <nathanielejones@hotmail.com>
2021-10-12 11:10:05 -0400

    added Switch statements to obtain task start points

--- script.js
+++ script.js
@@ -36,11 +36,6 @@ function createChart(e) {
 
 }
 
-// window.addEventListener("load", createChart); (keep here for when add-ons below are removed)
-// window.addEventListener("resize", createChart);
-
-// ADD-ONS START HERE
-
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -65,39 +60,90 @@ var taskFormHandler = function (event) {
     end: dayendInput
   };
 
+  alert("Task: " + taskDataObj.task + " - " + taskDataObj.start + " through " + taskDataObj.end);
+
+  let left = 0;
+  switch(taskDataObj.start) {
+    case "Mon":
+      left = 112;
+      break;
+    case "Tue":
+      left = 225;
+      break;
+    case "Wed":
+      left = 338;
+      break;
+    case "Thu":
+      left = 450;
+      break;
+    case "Fri":
+      left = 562;
+      break;
+    case "Sat":
+      left = 674;
+      break;
+    case "Sun":
+      left = 0;
+      break;
+    default:
+      left = 0;
+  }
+  // alert(left);
+
+  let width = 0;
+  switch(taskDataObj.end) {
+    case "Mon":
+      width = 112;
+      break;
+    case "Tue":
+      width = 225;
+      break;
+    case "Wed":
+      width = 338;
+      break;
+    case "Thu":
+      width = 450;
+      break;
+    case "Fri":
+      width = 562;
+      break;
+    case "Sat":
+      width = 674;
+      break;
+    case "Sun":
+      width = 0;
+      break;
+    default:
+      width = 0;
+  }
+  // alert(width);
+
+  let width1 = left + width;
+  alert("new task width should be: " + width1 + "px wide");
+
   // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
 };
 
 
-
-var createTaskEl = function (taskDataObj) {
-  // create list item
+var createTaskEl = function (taskDataObj, left, width) {
+  // Create list item
   var listItemEl = document.createElement("ul");
   listItemEl.className = "chart-bars";
-  let left = 0, width = 0;
-  
+  listItemEl.id = "tasks-to-do";
+  //let left = 0, width = 0;
 
-  // IMPLANT START from createChart for repurpose
-  listItemEl.style.left = `${left}px`;
-  listItemEl.style.width = `${width}px`;
-  listItemEl.style.opacity = .5;
-  //background-color: rgb(51, 168, 165); opacity: 1;
-  // if (listItemEl.type == "load") {
-  //   listItemEl.style.backgroundColor = listItemEl.dataset.color;
-  //   listItemEl.style.opacity = 1;
-  // }
-  // IMPLANT END from createChart for repurpose
-  
-
-  // create element to hold task info and add to list item
-  // STYLE NEEDS TO DYNAMICALLY GENERATE THE left AND width PROPERTIES: 
-  listItemEl.innerHTML = `<li data-duration="wed-thu" data-color="#b03532" class="task-item" style="left: 103px; width: 404px; background-color: rgb(51, 168, 165);">${taskDataObj.task}</li>`;
+  if (taskDataObj.type == "load") {
+    listItemEl.style.backgroundColor = listItemEl.dataset.color;
+    listItemEl.style.opacity = 1;
+  }
+ 
+  // Create element to hold task info and add to list item
+  // ORIGINAL KINDA WORKING - listItemEl.innerHTML = `<li data-duration="wed-sat" data-color="#b03532" class="task-item" style="left: 338px; width: 404px; background-color: rgb(51, 168, 165); opacity: 1;">${taskDataObj.task}</li>`;
+  listItemEl.innerHTML = `<li data-duration="${taskDataObj.start} - ${taskDataObj.end}" data-color="#b03532" class="task-item" style="left: ${left}px; width: ${width}px; background-color: rgb(51, 168, 165); opacity: 1;">${taskDataObj.task}</li>`;
   // listItemEl.innerHTML = `<li data-duration="${USER IN}" data-color="#b03532" class="task-item" style="left: ${CODE GEN}; width: ${CODE GEN}; background-color: rgb(51, 168, 165); opacity: 1;">${taskDataObj.task}</li>`;
-  
-  //listItemEl.appendChild();
 
-  // add entire list item to list
+  // Add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
   
 };
